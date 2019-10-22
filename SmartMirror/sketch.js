9bb0d957625cd4b3e33bac9a4876506658b9fcc2
ejiadedeji <ejiadedeji@gmail.com>
2019-10-22 17:29:26 -0500

    merged with aneesha

--- SmartMirror/sketch.js
+++ SmartMirror/sketch.js
@@ -3,13 +3,31 @@ let capture;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   capture = createCapture(VIDEO);
-  capture.hide();
-    
-   
+  capture.size(320, 240);
+  capture.hide(); 
 }
 
 function draw() {
+  background(192,192,192,120);
+  image(capture, 0, 0, windowWidth, windowHeight);
+   
+
   
-    image(capture, 0, 0, windowWidth, windowHeight);
- 
+    stroke(192,192,192,100); 
+   // Set the stroke width 
+    strokeWeight(15); 
+      
+    // Set the filled color 
+    
+      
+    
+    fill(192,192,192,10);
+  
+   rect(10, 10,windowWidth-40, windowHeight-100,10);
+  
+  
+  //filter(INVERT);
 }
+function mouseClicked() { 
+    save('cap.png');
+} 
