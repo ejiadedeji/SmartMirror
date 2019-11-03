430a4060ca93b5ca86325856314500fe22c0d1f0
tiwari-anubhav <55512991+tiwari-anubhav@users.noreply.github.com>
2019-11-03 15:41:30 -0600

    Push by anubhav

--- SmartMirror/sketch.js
+++ SmartMirror/sketch.js
@@ -1,33 +1,40 @@
 let capture;
+var spotifyPlayer;
+
+
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  capture.hide(); 
+createCanvas(windowWidth, windowHeight);
+capture = createCapture(VIDEO);
+capture.size(320, 240);
+    capture.hide(); 
+    spotifyPlayer = document.getElementById("spotify");
+    spotifyPlayer.style.display = 'none';
 }
 
 function draw() {
-  background(192,192,192,120);
-  image(capture, 0, 0, windowWidth, windowHeight);
-   
+background(192,192,192,120);
+image(capture, 0, 0, windowWidth, windowHeight);
+stroke(192,192,192,100); 
+// Set the stroke width 
+    strokeWeight(15);
 
-  
-    stroke(192,192,192,100); 
-   // Set the stroke width 
-    strokeWeight(15); 
-      
-    // Set the filled color 
     
-      
-    
-    fill(192,192,192,10);
-  
-   rect(10, 10,windowWidth-40, windowHeight-100,10);
-  
-  
-  //filter(INVERT);
+
 }
-function mouseClicked() { 
-    save('cap.png');
-} 
+function selfieCapture() { 
+save('cap.png');
+}
+
+function showMusic() {
+
+    if (spotifyPlayer.style.display === "none") {
+        spotifyPlayer.style.display = "block";
+    }
+    else {
+        spotifyPlayer.style.display = "none";
+    }
+}
+
+
+
